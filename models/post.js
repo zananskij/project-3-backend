156d1d88446fcfe97aacef12b9db2749712691ff
levonal <lwshabee@gmail.com>
2022-12-08 10:52:52 -0600

    backend

---
+++ models/post.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+const postSchema = new mongoose.Schema({
+    post: String,
+    img: String,
+    location: String,
+    date: {type: Date}
+})
+const Posts = mongoose.model('Posts', postSchema)
+module.exports = Posts
