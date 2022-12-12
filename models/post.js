ed094b493487ce4f885ed126cad4dcbd5c6cc9f5
zananskij <zananskij@gmail.com>
2022-12-12 13:40:34 -0500

    updated backend

--- models/post.js
+++ models/post.js
@@ -1,14 +1,11 @@
 const mongoose = require('mongoose')
 
 const postSchema = new mongoose.Schema({
-
-    post: String,
-    img: String,
-    location: String,
-    date: { type: String, default: Date }
-
+  post: String,
+  img: String,
+  location: { type: { type: String }, coordinates: [Number] },
+  date: { type: String, default: Date },
 })
-
 
 const Posts = mongoose.model('Posts', postSchema)
 module.exports = Posts
