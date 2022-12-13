83249503dacede63c6e8c3427e9e87625a82f5b6
zananskij <zananskij@gmail.com>
2022-12-13 13:20:56 -0500

    updated main for heroku

--- server.js
+++ server.js
@@ -79,4 +79,4 @@ app.put('/twitter/:id', (req, res) => {
 //___________________
 //Listener
 //___________________
-app.listen(3000, () => console.log('Listening on port:', 3000))
+app.listen(PORT, () => console.log('Listening on port:', PORT))
